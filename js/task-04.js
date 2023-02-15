17472b2b016672b6ebb84b39e31629c2b8a34524
ilona1009 <maliarenko.ilona@gmail.com>
2023-02-15 12:06:31 +0100

    Update task-04.js
    add

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,20 @@
+
+
+// const decrementRef = document.querySelector('button[data-action="decrement"]');
+// const incrementRef = document.querySelector('button[data-action="increment"]');
+// const valueRef = document.querySelector('#value');
+
+// decrementRef.addEventListener('click',function() {
+//     counterValue.decrement();
+//     valueRef.textContent = counterValue.value;
+// })
+
+// incrementRef.addEventListener('click',function() {
+//     counterValue.increment();
+//     valueRef.textContent = counterValue.value;
+// })
+
+
 const counterValue = {
     value: 0,
     decrement () {
@@ -9,16 +26,24 @@ this.value += 1;
 }   
 }
 
-const decrementRef = document.querySelector('button[data-action="decrement"]');
-const incrementRef = document.querySelector('button[data-action="increment"]');
+const btnDecrementRef = document.querySelector('button [data-action="decrement"]');
+const btnIncrementRef = document.querySelector('button [data-action="increment"]');
 const valueRef = document.querySelector('#value');
 
-decrementRef.addEventListener('click',function() {
+
+btnDecrementRef.addEventListener('click', function () {
     counterValue.decrement();
     valueRef.textContent = counterValue.value;
-})
+});
 
-incrementRef.addEventListener('click',function() {
+
+
+btnIncrementRef.addEventListener('click', function () {
     counterValue.increment();
-    valueRef.textContent = counterValue.value;
-})
+        valueRef.textContent = counterValue.value
+});
+
+
+    
+
+
