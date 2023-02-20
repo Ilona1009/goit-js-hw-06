e4f78e701757b577712ad9540b62602d70d98161
ilona1009 <maliarenko.ilona@gmail.com>
2023-02-20 16:18:16 +0100

    a
    add

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,36 @@
-const btnRef = document.querySelector('.change-color');
-const spanRef = document.querySelector('.color');
-const bodyRef = document.body;
+// const btnRef = document.querySelector('.change-color');
+// const spanRef = document.querySelector('.color');
+// const bodyRef = document.body;
 
-btnRef.addEventListener('click', clickBtn)
+// btnRef.addEventListener('click', clickBtn)
 
-function clickBtn (event){
+// function clickBtn (event){
 
-const NewColor = getRandomHexColor();
-bodyRef.style.background = NewColor;
-spanRef.textContent = NewColor;
+// const NewColor = getRandomHexColor();
+// bodyRef.style.background = NewColor;
+// spanRef.textContent = NewColor;
 
 
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`}
+
+const bodyRef = document.querySelector('body');
+const btnRef = document.querySelector('button');
+const colorRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', newColorRef);
+
+
+function newColorRef() {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    colorRef.textContent = getRandomHexColor();
 }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,24 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const inputRef = document.querySelector('[number]');
+const btnCreateRef = document.querySelector('button[data-create]');
+const btnDestroyRef = document.querySelector('button[data-destroy]');
+const idBoxesRef = document.querySelector('#boxes');
+
+function createBoxes(inputRef) {
+  for (let index = 0; index < inputRef; index += 1) {
+    console.log('gg')
+
+    const box = document.createElement('div');
+  }
 }
+
+btnCreateRef.addEventListener('submit', createBoxes(inputRef));
+
+
+// console.log(createBoxes(90))
+console.log(inputRef)
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
