874f43c831717e19faf5d8f316365ea20a29d79b
ilona1009 <maliarenko.ilona@gmail.com>
2023-02-21 16:35:49 +0100

    z
    add

--- js/task-10.js
+++ js/task-10.js
@@ -1,24 +1,26 @@
-const inputRef = document.querySelector('[number]');
+const inputRef = document.querySelector('input');
 const btnCreateRef = document.querySelector('button[data-create]');
 const btnDestroyRef = document.querySelector('button[data-destroy]');
 const idBoxesRef = document.querySelector('#boxes');
 
-function createBoxes(inputRef) {
-  for (let index = 0; index < inputRef; index += 1) {
-    console.log('gg')
+const createBoxes = amount => {
+    const targetInput = amount .currentTarget.value;
 
-    const box = document.createElement('div');
+  for (let index = 0; index < targetInput; index += 1) {
+     idBoxesRef.insertAdjacentElement("beforeend", box)
+;
   }
+  
 }
 
-btnCreateRef.addEventListener('submit', createBoxes(inputRef));
+const box = document.createElement('div');
+    box.setAttribute('id', 'boxes');
+    box.style =" width: 30px; hight: 30px";
+    box.style.color = getRandomHexColor();
 
 
-// console.log(createBoxes(90))
-console.log(inputRef)
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
+console.log(box)
